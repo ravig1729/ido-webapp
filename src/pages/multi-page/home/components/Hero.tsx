1c038d9919d865c84495bb3b51987cb4280e378a
p0d00td <prasad.dola@walmart.com>
2025-04-19 02:51:04 +0530

    update content

--- src/pages/multi-page/home/components/Hero.tsx
+++ src/pages/multi-page/home/components/Hero.tsx
@@ -1,14 +1,88 @@
-import {useRef} from 'react'
-import hero1 from '@/assets/img/shape/vl-hero-shape-1.1.png'
-import hero2 from '@/assets/img/shape/vl-hero-shape-1.2.png'
-import titleIcon from '@/assets/img/icons/vl-sub-title-icon.svg'
-import {FaAngleLeft, FaAngleRight, FaArrowRight, FaFacebookF, FaInstagram, FaTwitter} from "react-icons/fa6";
-import {FaGithub} from "react-icons/fa";
-import {Col, Container, Row} from 'react-bootstrap'
-import Slider from 'react-slick'
+import { useRef } from 'react';
+import hero1 from '@/assets/img/shape/vl-hero-shape-1.1.png';
+import hero2 from '@/assets/img/shape/vl-hero-shape-1.2.png';
+import titleIcon from '@/assets/img/icons/vl-sub-title-icon.svg';
+import { FaAngleLeft, FaAngleRight, FaArrowRight, FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa';
+import { Col, Container, Row } from 'react-bootstrap';
+import Slider from 'react-slick';
+
+// Reusable component for Hero Shapes
+const HeroShapes = () => (
+    <>
+        <div className="vl-hero-shape shape-1">
+            <img src={hero1} alt="hero1" />
+        </div>
+        <div className="vl-hero-shape shape-2">
+            <img src={hero2} alt="hero2" />
+        </div>
+    </>
+);
+
+// Reusable component for Hero Social Links
+const HeroSocial = () => (
+    <div className="vl-hero-social d-none d-lg-block">
+        <h4 className="title">Follow Us:</h4>
+        <div className="vl-hero-social-icon">
+            <ul>
+                <li>
+                    <a className="d-flex align-items-center justify-content-center" href="#">
+                        <FaFacebookF />
+                    </a>
+                </li>
+                <li>
+                    <a className="d-flex align-items-center justify-content-center" href="#">
+                        <FaInstagram />
+                    </a>
+                </li>
+                <li>
+                    <a className="d-flex align-items-center justify-content-center" href="#">
+                        <FaTwitter />
+                    </a>
+                </li>
+                <li>
+                    <a className="d-flex align-items-center justify-content-center" href="#">
+                        <FaGithub />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+);
+
+// Reusable component for Hero Section Title
+const HeroSectionTitle = () => (
+    <div className="vl-hero-section-title">
+        <h5 className="vl-subtitle">
+            <span>
+                <img src={titleIcon} alt="titleIcon" />
+            </span>{' '}
+            <span>Welcome to</span>
+        </h5>
+        <h1 className="vl-title text-anime-style-3">Indigenous Development Organization</h1>
+        <p>
+            Your contribution can help achieve a solution
+        </p>
+        <div className="vl-hero-btn-container" >
+            <div className="vl-hero-btn">
+                <a href="/pages/contact" className="header-btn1">
+                    Become a Volunteer<span>
+                        <FaArrowRight />
+                    </span>
+                </a>
+            </div>
+            <div className="vl-hero-btn">
+                <a href="/pages/contact" className="header-btn1">
+                    Donate now <span>
+                        <FaArrowRight />
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+);
 
 const Hero = () => {
-
     const settings = {
         draggable: true,
         autoplay: true,
@@ -25,109 +99,46 @@ const Hero = () => {
 
     const sliderRef = useRef<Slider | null>(null);
 
-
     return (
         <div className="vl-banner p-relative fix">
             <Slider ref={sliderRef} {...settings} className="slider-active-1">
                 <div className="vl-hero-slider vl-hero-bg slick-slide slick-current slick-active">
-                    <div className="vl-hero-shape shape-1">
-                        <img src={hero1} alt='hero1'/>
-                    </div>
-                    <div className="vl-hero-shape shape-2">
-                        <img src={hero2} alt='hero2'/>
-                    </div>
-                    <div className="vl-hero-social d-none d-lg-block">
-                        <h4 className="title">Follow Us:</h4>
-                        <div className="vl-hero-social-icon">
-                            <ul>
-                                <li><a className='d-flex align-items-center justify-content-center'
-                                       href="#"><FaFacebookF className="fa-brands fa-facebook-f"/></a></li>
-                                <li><a className='d-flex align-items-center justify-content-center'
-                                       href="#"><FaInstagram className="fa-brands fa-instagram"/></a></li>
-                                <li><a className='d-flex align-items-center justify-content-center' href="#"><FaTwitter
-                                    className="fa-brands fa-twitter"/></a></li>
-                                <li><a className='d-flex align-items-center justify-content-center' href="#"><FaGithub
-                                    className="fa-brands fa-github"/></a></li>
-                            </ul>
-                        </div>
-                    </div>
+                    <HeroShapes />
+                    <HeroSocial />
                     <Container>
                         <Row>
                             <Col lg={7}>
-                                <div className="vl-hero-section-title">
-                                    <h5 className="vl-subtitle"><span><img src={titleIcon}
-                                                                           alt='titleIcon'/></span> Recognizing Our
-                                        Disaster Relief Heroes</h5>
-                                    <h1 className="vl-title text-anime-style-3">Disaster Relief Funding</h1>
-                                    <p>In times of crisis, when disaster strikes and hope seems lost, <br/> there emerge
-                                        unsung heroes who rise to the occasion.</p>
-                                    <div className="vl-hero-btn">
-                                        <a href="/pages/contact" className="header-btn1">Join The Relief
-                                            Effort <span><FaArrowRight/></span></a>
-                                    </div>
-                                </div>
+                                <HeroSectionTitle />
                             </Col>
-                            <Col lg={5}/>
+                            <Col lg={5} />
                         </Row>
                     </Container>
                 </div>
 
                 <div className="vl-hero-slider vl-hero-bg slick-slide slick-current slick-active">
-                    <div className="vl-hero-shape shape-1">
-                        <img src={hero1} alt='hero1'/>
-                    </div>
-                    <div className="vl-hero-shape shape-2">
-                        <img src={hero2} alt='hero2'/>
-                    </div>
-                    <div className="vl-hero-social d-none d-lg-block">
-                        <h4 className="title">Follow Us:</h4>
-                        <div className="vl-hero-social-icon">
-                            <ul>
-                                <li><a className='d-flex align-items-center justify-content-center'
-                                       href="#"><FaFacebookF/></a></li>
-                                <li><a className='d-flex align-items-center justify-content-center'
-                                       href="#"><FaInstagram/></a></li>
-                                <li><a className='d-flex align-items-center justify-content-center'
-                                       href="#"><FaTwitter/></a></li>
-                                <li><a className='d-flex align-items-center justify-content-center' href="#"><FaGithub/></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                    <HeroShapes />
+                    <HeroSocial />
                     <Container>
                         <Row>
                             <Col lg={7}>
-                                <div className="vl-hero-section-title">
-                                    <h5 className="vl-subtitle"><span><img src={titleIcon}
-                                                                           alt='titleIcon'/></span> Recognizing Our
-                                        Disaster Relief Heroes</h5>
-                                    <h1 className="vl-title text-anime-style-3">Disaster Relief Funding</h1>
-                                    <p>In times of crisis, when disaster strikes and hope seems lost, <br/> there emerge
-                                        unsung heroes who rise to the occasion.</p>
-                                    <div className="vl-hero-btn">
-                                        <a href="/pages/contact" className="header-btn1">Join The Relief
-                                            Effort <span><FaArrowRight/></span></a>
-                                    </div>
-                                </div>
+                                <HeroSectionTitle />
                             </Col>
-                            <Col lg={5}/>
+                            <Col lg={5} />
                         </Row>
                     </Container>
                 </div>
             </Slider>
 
             <div className="vl-arrow">
-        <span className="prev-arow slick-arrow" onClick={() => sliderRef.current?.slickPrev()}>
-          <FaAngleRight/>
-        </span>
+                <span className="prev-arow slick-arrow" onClick={() => sliderRef.current?.slickPrev()}>
+                    <FaAngleRight />
+                </span>
                 <span className="next-arow slick-arrow" onClick={() => sliderRef.current?.slickNext()}>
-          <FaAngleLeft/>
-        </span>
+                    <FaAngleLeft />
+                </span>
             </div>
         </div>
+    );
+};
 
-
-    )
-}
-
-export default Hero
+export default Hero;
