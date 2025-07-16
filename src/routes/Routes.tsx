5fb0b2bc51b09fc9b77d102b4aa8830e8784d6f0
ravig1729 <ravigedela1729@gmail.com>
2025-07-16 23:42:39 +0530

    -- updated code

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,17 +1,14 @@
 import { lazy } from "react";
 
-const Home = lazy(() => import('@/pages/multi-page/home'))
+const Home = lazy(() => import('@/pages/home'))
 const About = lazy(() => import('@/pages/about'))
-
 const Reports = lazy(() => import('@/pages/reports-left'))
 const Partners = lazy(() => import('@/pages/partners'))
 const Contact = lazy(() => import('@/pages/contact'))
 const Donation = lazy(() => import('@/pages/donation'))
 const Error404 = lazy(() => import('@/pages/other-pages/error-404'))
 const Faq = lazy(() => import('@/pages/other-pages/faq'))
-const Service = lazy(() => import('@/pages/other-pages/service'))
 const Team = lazy(() => import('@/pages/other-pages/volunteer'))
-
 const Health = lazy(() => import('@/pages/work/health'));
 const Nutrition = lazy(() => import('@/pages/work/nutrition'));
 const Education = lazy(() => import('@/pages/work/education'));
@@ -23,55 +20,7 @@ const multiPageRoutes = [
         name: 'multi-page-home',
         element: <Home/>,
     },
-    // {
-    //     path: '/multi-page/animal-rescue',
-    //     name: 'multi-page-animal-rescue',
-    //     element: <MultiPageAnimalRescue/>,
-    // },
-    // {
-    //     path: '/multi-page/human-rights',
-    //     name: 'multi-page-human-rights',
-    //     element: <MultiPageHumanRights/>,
-    // },
-    // {
-    //     path: '/multi-page/senior-citizen',
-    //     name: 'multi-page-senior-citizen',
-    //     element: <MultiPageSeniorCitizen/>,
-    // },
-    // {
-    //     path: '/multi-page/water-aid',
-    //     name: 'multi-page-water-aid',
-    //     element: <MultiPageWaterAid/>,
-    // },
 ]
-
-// const singlePageRoutes = [
-//     {
-//         path: '/home-single',
-//         name: 'multi-page-home',
-//         element: <SinglePageHome/>,
-//     },
-//     {
-//         path: '/animal-rescue',
-//         name: 'single-page-animal-rescue',
-//         element: <SinglePageAnimalRescue/>,
-//     },
-//     {
-//         path: '/human-rights',
-//         name: 'single-page-human-rights',
-//         element: <SinglePageHumanRights/>,
-//     },
-//     {
-//         path: '/senior-citizen',
-//         name: 'single-page-senior-citizen',
-//         element: <SinglePageSeniorCitizen/>,
-//     },
-//     {
-//         path: '/water-aid',
-//         name: 'single-page-water-aid',
-//         element: <SinglePageWaterAid/>,
-//     },
-// ]
 
 const otherRoutes = [
     {
@@ -79,66 +28,11 @@ const otherRoutes = [
         name: 'about',
         element: <About/>,
     },
-    // {
-    //     path: '/event',
-    //     name: 'event',
-    //     element: <Event/>,
-    // },
-    // {
-    //     path: '/event-left',
-    //     name: 'event-left',
-    //     element: <EventLeft/>,
-    // },
-    // {
-    //     path: '/event-right',
-    //     name: 'event-right',
-    //     element: <EventRight/>,
-    // },
-    // {
-    //     path: '/event-single',
-    //     name: 'event-single',
-    //     element: <EventSingle/>,
-    // },
     {
         path: '/reports',
         name: 'reports',
         element: <Reports/>,
     },
-    // {
-    //     path: '/blog-left',
-    //     name: 'blog-left',
-    //     element: <BlogLeft/>,
-    // },
-    // {
-    //     path: '/blog-right',
-    //     name: 'blog-right',
-    //     element: <BlogRight/>,
-    // },
-    // {
-    //     path: '/blog-single',
-    //     name: 'blog-single',
-    //     element: <BlogSingle/>,
-    // },
-    // {
-    //     path: '/cause',
-    //     name: 'cause',
-    //     element: <Cause/>,
-    // },
-    // {
-    //     path: '/cause-left',
-    //     name: 'cause-left',
-    //     element: <CauseLeft/>,
-    // },
-    // {
-    //     path: '/cause-right',
-    //     name: 'cause-right',
-    //     element: <CauseRight/>,
-    // },
-    // {
-    //     path: '/cause-single',
-    //     name: 'cause-single',
-    //     element: <CauseSingle/>,
-    // },
     {
         path: '/partners',
         name: 'partners',
@@ -163,11 +57,6 @@ const otherRoutes = [
         path: '/pages/faq',
         name: 'pages-faq',
         element: <Faq/>,
-    },
-    {
-        path: '/pages/service',
-        name: 'pages-service',
-        element: <Service/>,
     },
     {
         path: '/pages/team',
